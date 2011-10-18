1d1179a0f1bf306ca2b10c03efa38b48cbaba0d3
Rob Hoelz <rob@hoelz.ro>
2011-10-17 22:16:31 -0500

    Add LocalBrew plugin back

---
+++ lib/Dist/Zilla/Plugin/LocalBrew.pm
@@ -0,0 +1,17 @@
+package Dist::Zilla::Plugin::LocalBrew;
+
+use Moose;
+
+extends 'Dist::Zilla::Plugin::Test::LocalBrew';
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
