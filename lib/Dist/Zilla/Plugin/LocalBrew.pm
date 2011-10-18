41bc1ce2dea93e832d2df0327c6e7bf884f2a0ca
Rob Hoelz <rob@hoelz.ro>
2011-10-17 22:18:07 -0500

    Warn about deprecated LocalBrew

--- lib/Dist/Zilla/Plugin/LocalBrew.pm
+++ lib/Dist/Zilla/Plugin/LocalBrew.pm
@@ -4,6 +4,13 @@ use Moose;
 
 extends 'Dist::Zilla::Plugin::Test::LocalBrew';
 
+before register_component => sub {
+    warn "!!! [LocalBrew] is deprecated and may be removed in a future release; replace it with [Test::LocalBrew]\n";
+};
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
 1;
 
 __END__
